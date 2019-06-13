d920687329aa9c73b42eadb4c51ed03421682dfe
LIZETHVERA <lizeth.veraro@gmail.com>
2019-06-13 10:46:06 -0700

    Fixed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,5 @@
 var db = require("../models");
 
-// var authCheck = function(req, res, next) {
-//   if (!req.user) {
-//     res.redirect("/");
-//   } else {
-//     next();
-//   }
-// };
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
@@ -19,12 +11,6 @@ module.exports = function(app) {
     });
   });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin
   app.get("/sign-up", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("sign-up", {
@@ -34,11 +20,6 @@ module.exports = function(app) {
     });
   });
 
-<<<<<<< HEAD
-=======
->>>>>>> parent of 3917cbd... Revert "Merge pull request #18 from JesseBoe/passport"
-=======
->>>>>>> origin
   //This is an example of enforcing a user to be logged in. If not logged in
   //the user is redirected to root
 
@@ -46,10 +27,6 @@ module.exports = function(app) {
   //   res.render("profile");
   // })
 
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/master
-=======
->>>>>>> parent of 3917cbd... Revert "Merge pull request #18 from JesseBoe/passport"
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
